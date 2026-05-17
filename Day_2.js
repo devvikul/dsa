c2acf4588f113e4b3c68c6e73c0ba10db69c78bb
Vikul <info.vikuldev@gmail.com>
2026-05-17 23:11:44 +0530

    Day 2 : Valid Parentheses

---
+++ Day_2.js
@@ -0,0 +1,36 @@
+// Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+var isValid = function(s) {
+    let stack = [];
+
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+
+        // Opening brackets
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } 
+        // Closing brackets
+        else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
+
+console.log(isValid("()[]{}"))
